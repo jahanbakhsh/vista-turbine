bc4d2cbf92a39747d36ceab190fa5a06c375a7e1
jahanbakhsh.m <jhanbksh.mohammadi@gmail.com>
2018-11-11 12:56:23 +0330

    gunicorn config

--- configs/gunicorn/gunicorn.bash
+++ configs/gunicorn/gunicorn.bash
@@ -1,30 +1,14 @@
-NAME="VistaTurbine_gunicorn"                                  # Name of the application
-DJANGODIR=/Vista/vista-turbine            # Django project directory
-SOCKFILE=/Vista/vista-turbine/run/gunicorn.sock  # we will communicte using this unix socket
-USER=root                                        # the user to run as
-GROUP=vista                                     # the group to run as
-NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
-DJANGO_SETTINGS_MODULE=VistaTurbine.settings             # which settings file should Django use
-DJANGO_WSGI_MODULE=VistaTurbine.wsgi                     # WSGI module name
+#!/bin/bash
+set -e
+
+NAME="VistaTurbine Gunicorn"
+VistaTurbine_ENV_NAME="venv"
+VistaTurbine_PATH=/VistaTurbine
+DJANGO_WSGI_MODULE=VistaTurbine.wsgi
 
 echo "Starting $NAME as `whoami`"
 
-# Activate the virtual environment
-cd $DJANGODIR
-source ../../venv/bin/activate
-export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
-export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+cd $SDP_PATH
 
-# Create the run directory if it doesn't exist
-RUNDIR=$(dirname $SOCKFILE)
-test -d $RUNDIR || mkdir -p $RUNDIR
-
-# Start your Django Unicorn
-# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
-exec ../bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
-  --name $NAME \
-  --workers $NUM_WORKERS \
-  --user=$USER --group=$GROUP \
-  --bind=unix:$SOCKFILE \
-  --log-level=debug \
---log-file=-
+exec supervisord
+exec gunicorn -c $VistaTurbine_PATH/configs/gunicorn/gunicorn.conf.py ${DJANGO_WSGI_MODULE}

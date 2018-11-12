0a097d30dcf0683baf21e248f01d7cf562d6eb65
jahanbakhsh.m <jhanbksh.mohammadi@gmail.com>
2018-11-12 12:10:33 +0330

    website content

--- VistaTurbine/settings.py
+++ VistaTurbine/settings.py
@@ -164,6 +164,7 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'static')
 ]
